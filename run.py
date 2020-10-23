ce6a32cebade8747034c6efa421ee0ec0d8284af
Alan Beck <beckalan303@gmail.com>
2020-10-23 15:42:49 -0300

    add a screen home and bootstrap css

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'hola mundo'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
